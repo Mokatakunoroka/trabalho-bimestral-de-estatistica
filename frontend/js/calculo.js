dcbb965755ab8a34dbd7a7f672efc1463b73e836
Luis Fernando <deliciasfrancesas16@gmail.com>
2026-05-04 14:50:44 -0300

    adiciona inputs dinâmicos e estrutura para resultados no cálculo no HTML.

---
+++ frontend/js/calculo.js
@@ -0,0 +1,18 @@
+//Esssa constante recebe o elemento <section> dentro do main, que tem o id "info-calculo"
+const sessao_Main = document.getElementById("info-calculo");
+
+//Esse for, faz um loop 7 vezes
+for (let i = 0; i < 7; i++)
+{
+    //Uso a função insertAdjacentHTML para adicionar no fim do elemento os inputs
+    sessao_Main.insertAdjacentHTML("beforeend", 
+    `
+    <input
+    required
+    type="text"
+    id="input${i}"
+    placeholder="Digite o número...">
+
+    <br>
+    `);
+}
