745df79d48e17572534b77b71dce2213a0e088eb
salgadoH2 <joaovitormartins339@gmail.com>
2026-05-16 12:25:40 -0300

    mudei o nome do calculo.js para inicio.js

--- frontend/js/inicio.js
+++ frontend/js/inicio.js
@@ -1,6 +1,5 @@
 //Esssa constante recebe o elemento <section> dentro do main, que tem o id "info-calculo"
 const sessao_Main = document.getElementById("info-calculo");
-
 //Esse for, faz um loop 7 vezes
 for (let i = 0; i < 7; i++)
 {
